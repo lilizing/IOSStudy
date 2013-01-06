ab79c8dea427062eeba05d587d1990c5c671c0b0
lili <lilizing@qq.com>
2013-01-06 15:55:11 +0800

    IOSStudy工程创建
    用于今后对IOS学习研究记录

---
+++ IOSStudy/IOSStudy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSStudy
+//
+//  Created by lili on 13-1-6.
+//  Copyright (c) 2013年 lili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSStudy/IOSStudyTests/IOSStudyTests.h
@@ -0,0 +1,13 @@
+//
+//  IOSStudyTests.h
+//  IOSStudyTests
+//
+//  Created by lili on 13-1-6.
+//  Copyright (c) 2013年 lili. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IOSStudyTests : SenTestCase
+
+@end
